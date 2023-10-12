d42d00f6868f7fe3600cbc8a51187e6c50d2c41c
Your Name <you@example.com>
2023-10-12 10:01:15 +0545

    modifed datetimejs

--- portfolio/staticfiles/admin/js/DateTimeShortcuts.js
+++ portfolio/staticfiles/admin/js/DateTimeShortcuts.js
@@ -189,11 +189,206 @@
         
             }
 
-            clock_box.style.top=Math.max(0,findPosY(clock_link)-30 +'px'
+            clock_box.style.top=Math.max(0,findPosY(clock_link)-30 )+'px';
             // show the clock box
+            clock_box.style.display='block';
+            document.addEventListener('click', DateTimeShortcuts.dismissClockFunc[num]);
             
-            )
-        }
+        },
+        dismissClock: function(num){
+            document.getElementById(DateTimeShortcuts.clockDivName+num).style.display='none';
+            document.removeEventListener('click',DateTimeShortcuts.dismissClockFunc[num]);
+        },
+        handleClockQuickLink: function(num,val){
+            var d;
+            if (val===-1){
+                d=DateTimeShortcuts.now();
+            }else{
+                d=new Date(1970,1,1,val,0,0,0);
+            }
+            DateTimeShortcuts.clockInputs[num].value=d.strftime(get_format('TIME_INPUT_FORMATS')[0]);
+            DateTimeShortcuts.clockInputs[num].focus();
+            DateTimeShortcuts.dismissClock(num);
+        },
+        // Add calendar widget to a given field
+        addCalendar: function(inp){
+            var num=DateTimeShortcuts.calendar.length;
 
-    }
-})
+            DateTimeShortcuts.calendarInputs[num]=inp;
+            DateTimeShortcuts.dismissCalendarFunc[num]=function(){DateTimeShortcuts.dismissCalendar(num); return true};
+
+            // shortcuts links (calendar item and "Today " link)
+            var shortcurs_span=document.createElement('span');
+            shortcurs_span.className=DateTimeShortcuts.shortCutsClass;
+            inp.parentNode.insertBefore(shortcurs_span,inp.nextSibling);
+            var today_link = document.createElement('a');
+            today_link.setAttribute('href','#');
+            today_link.appendChild(document.createTextNode(gettext('Today')));
+            today_link.addEventListener('click',function(e){
+                e.preventDefault();
+                DateTimeShortcuts.handleCalendarQuickLink(num,0);
+            });
+            var cal_link=document.createElement('a');
+            cal_link.setAttribute('href','#');
+            cal_link.id=DateTimeShortcuts.calendarLinkName+num;
+            cal_link.addEventListener('click',function(e){
+                e.preventDefault();
+                // avoid triggering the document click handler to dismiss the calendar
+                e.stopPropagation();
+                DateTimeShortcuts.openCalendar(num);
+            });
+            quickElement(
+                'span',cal_link,'',
+                'class','date-icon',
+                'title',gettext('choose a Date')
+            );
+            shortcurs_span.appendChild(document.createTextNode('\u00A0'));
+            shortcurs_span.appendChild(today_link);
+            shortcurs_span.appendChild(document.createTextNode('\u00A0|\u00A0'));
+            shortcurs_span.appendChild(cal_link);
+
+            // create a calendarbox div
+
+            // markup looks like
+            // <div id="calendarbox3" class="calendarbox module">
+            //  <h2 >
+            //      <a href="#" class="link-previous">&lsaquo;</a>
+            //      <a href="#" class="link-next">&rsaquo;</a> February 2003
+            // </h2>
+            // <div class="calendar" id="calendarin3">
+            //      <!--(cal)-->
+            // </div>
+            // <div class="calendar-shortcuts">
+            //      <a href-"#">Yesterday</a> | <a href="#">Today</a> |
+            // </div>
+            // <p class="calendar-cancel"><a href="#">Cancel</a></p>
+            // </div>
+            var cal_box=document.createElement('div');
+            cal_box.style.display='none';
+            cal_box.style.position='absolute';
+            cal_box.className='calendarbox module';
+            cal_box.setAttribute('id',DateTimeShortcuts.calendarDivName1+num);
+            document.body.appendChild(cal_box);
+            cal_box.addEventListener('click',function(e){e.stopPropagation();});
+
+            // next-prev links
+            var cal_nav=quickElement('div',cal_box);
+            var cal_nav_prev=quickElement('a',cal_nav,'<','href','#');
+            cal_nav_prev.className='calendarnav-previous';
+            cal_nav_prev.addEventListener('click',function(e){
+                e.preventDefault();
+                DateTimeShortcuts.drawPrev(num);
+            });
+            var cal_nav_next=quickElement('a',cal_nav,'>','href','#');
+            cal_nav_next.className='calendarnav-next';
+            cal_nav_next.addEventListener('click',function(e){ 
+                e.preventDefault();
+                DateTimeShortcuts.drawNext(num);
+            });
+            // main box
+            var cal_main=quickElement('div',cal_box,'','id',DateTimeShortcuts.calendarDivName2+num);
+            cal_main.className='calendar';
+            DateTimeShortcuts.calendar[num]=new Calendar(DateTimeShortcuts.calendarDivName2+num,DateTimeShortcuts.handleCalendarCallback(num));
+            DateTimeShortcuts.calendar[num].drawCurrent();
+
+            // calendar shortcuts
+            var shortcuts=quickElement('div',cal_box);
+            shortcuts.className='calendar-shortcuts';
+            var day_link=quickElement('a',shortcuts,gettext('Yesterday'),'href','#');
+            day_link.addEventListener('click',function(e){
+                e.preventDefault();
+                DateTimeShortcuts.handleCalendarQuickLink(num,-1);
+            });
+            shortcuts.appendChild(document.createTextNode('\u00A0|\u00A0'));
+            day_link=quickElement('a', shortcuts,gettext('Today'),'href','#');
+            day_link.addEventListener('click',function(e){
+                e.preventDefault();
+                DateTimeShortcuts.handleCalendarQuickLink(num,+1);
+            });
+
+            // cancel bar
+            var cancel_p=quickElement('p',cal_box);
+            cancel_p.className='calendar-cancel';
+            var cancel_link=quickElement('a',cancel_p,gettext('Cancel'),'href','#');
+            cancel_link.addEventListener('click',function(e){
+                e.preventDefault();
+                DateTimeShortcuts.dismissCalendar(num);
+            });
+            document.addEventListener('keyup',function(event){
+                if (event.which===27){
+                    // ESc key closes popup
+                    DateTimeShortcuts.dismissCalendar(num);
+                    event.preventDefault();
+                }
+            });
+
+
+
+        },
+        openCalendar: function(num){
+            var cal_box=document.getElementById(DateTimeShortcuts.calendarDivName1+num);
+            var cal_link=document.getElementById(DateTimeShortcuts.calendarLinkName+num);
+            var inp=DateTimeShortcuts.calendarInputs[num];
+            // Determine if the current value in the input has a valid date.
+            // If so, draw the calendar with that date's year and month.
+            if(inp.value){
+                var format=get_format('DATE_INPUT_FORMATS')[0];
+                var selected=inp.value.strptime(format);
+                var year=selected.getUTCFullYear();
+                var month=selected.getUTCMonth()+1;
+                var re=/\d{4}/;
+                if (re.test(year.toString())&&month>=1 && month<=12){
+                    DateTimeShortcuts.calendar[num].drawDate(month,year,selected);
+                }
+            }
+            // recalculate the clockboz position
+            // it is ltr or rtl layout?
+            if(window.getComputedStyle(document.body).direction !== 'rtl'){
+                cal_box.style.left=findPosX(cal_link)+17 +'px';
+            }else{
+                // since style's width is in em, it'd be tough to calculate
+                // px value of it.let's use an estimated px for now
+                // TODO: IE returns wrong value for findPosX when in rtl mode
+                // (it returns as it was left aligned), needs to be fixed.
+                cal_box.style.left=findPosX(cal_link)-180 +'px';
+            }
+            cal_box.style.top=Math.max(0,findPosY(cal_link)-75)+'px';
+
+            cal_box.style.display='block';
+            document.addEventListener('click',DateTimeShortcuts.dismissCalendarFunc[num]);
+        },
+        dismissCalendar:function(num){
+            document.getElementById(DateTimeShortcuts.calendarDivName1+num).style.display='none';
+            document.removeEventListener('click',DateTimeShortcuts.dismissCalendarFunc[num]);
+        },
+        drawPrev:function(num){
+            DateTimeShortcuts.calendar[num].drawPreviousMonth();
+        },
+        drawNext:function(num){
+            DateTimeShortcuts.calendar[num].drawNextMonth();
+        },
+        handleCalendarCallback:function(num){
+            var format=get_format('DATE_INPUT_FORMATS')[0];
+            // the format needs to escaped a little
+            format=format.replace('\\','\\\\')
+                .replace('\r','\\r')
+                .replace('\n','\\n')
+                .replace('\t','\\t')
+                .replace("'","\\'");
+            return function(y,m,d){
+                DateTimeShortcuts.calendarInputs[num].value=new Date(y,m-1,d).strftime(format);
+                DateTimeShortcuts.calendarInputs[num].focus();
+                document.getElementById(DateTimeShortcuts.calendarDivName1+num).style.display='none';
+            } ;
+        },
+        handleCalendarQuickLink:function(num,offset){
+            var d=DateTimeShortcuts.now();
+            d.setDate(d.getDate()+offset);
+            DateTimeShortcuts.calendarInputs[num].value=d.strftime(get_format('DATE_INPUT_FORMATS')[0]);
+            DateTimeShortcuts.calendarInputs[num].focus();
+            DateTimeShortcuts.dismissCalendar(num);
+        }
+    };
+    window.addEventListener('load',DateTimeShortcuts.init);
+    window.DateTimeShortcuts=DateTimeShortcuts;
+})();
